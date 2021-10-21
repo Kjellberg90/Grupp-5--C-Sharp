0b3e64643b73d00a466400c6751f39b35180808a
PennyDanneborn <92095640+PennyDanneborn@users.noreply.github.com>
2021-10-21 09:44:33 +0200

    Add files via upload

---
+++ Penny/VolvoDesign/VolvoDesign/VolvoDesign/Suv.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolvoDesign
+{
+    enum Petrol
+    {
+        Low,
+        Medium,
+        Full
+    }
+    class Suv: Car
+    {
+        private void OffRoadMode()
+        {
+            Console.WriteLine("Car is now in OffRoad-mode");
+                                
+        }
+
+        private void EcoMode()
+        {
+            Console.WriteLine("Car is now in ECO-mode");
+        }
+
+        private int PetrolLevel
+        {
+            get
+            {
+                return PetrolLevel;
+            }
+
+            set
+            {
+                Console.WriteLine(Petrol.Full);
+            }
+        }
+    }
+}
